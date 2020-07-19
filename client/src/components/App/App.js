dc508386ece58cc7a4a0e4c221af2dccc0f88463
ricktjwong <rickwtj@gmail.com>
2020-07-19 22:06:14 +0800

    Fetch json data and render on results component
    - Use alpha vantage as server mockup data to render chart on frontend
    - This will be replaced by data from own server to showcase backtested
    results and returns from weights used using the Black Litterman model

---
+++ client/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResultsComponent from '../Results'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hi
+    <ResultsComponent />
+    </div>
+  );
+}
+
+export default App;
