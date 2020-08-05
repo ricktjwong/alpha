838bc722373fa401c99f340da0a5a579eeec64cb
ricktjwong <rickwtj@gmail.com>
2020-08-05 15:38:39 +0800

    Integrate backtest and alloc apis, add caption

--- client/src/components/apiHandler.js
+++ client/src/components/apiHandler.js
@@ -1,34 +1,93 @@
 import _ from 'lodash';
 
-export const getApiData = (props) => {
-  const { tickers, setEntireDomain, setChartData, setRenderLoad } = props;
-  // Pass tickers to backend and get weights and backtest
-  // as response
-  const apiKey = process.env.REACT_APP_ALPHAVANTAGE_APIKEY;
-  const ticker = 'AAPL';
-  const urlString =
-    'https://www.alphavantage.co/query?function=' +
-    'TIME_SERIES_DAILY_ADJUSTED&' +
-    'symbol=' +
-    ticker +
-    '&outputsize=full&apikey=' +
-    apiKey;
-  fetch(urlString)
+export const getTickers = (setSearchTickers) => {
+  fetch(`${process.env.REACT_APP_DB_URL}/application/ticker`)
     .then((res) => res.json())
-    .then((json) => {
-      let stockData = json['Time Series (Daily)'];
-      const labels = Object.keys(stockData).reverse();
-      const values = Object.values(stockData).reverse();
-      const data = values.map((x) => parseFloat(x['5. adjusted close']));
-      const final = [];
-      for (var i = 0; i < data.length; i++) {
-        final.push({ x: new Date(labels[i]), y: data[i] });
-      }
-      setEntireDomain({
-        y: [_.minBy(final, (d) => d.y).y, _.maxBy(final, (d) => d.y).y],
-        x: [final[0].x, _.last(final).x],
-      });
-      setChartData(final);
-      setRenderLoad(false);
+    .then((data) => {
+      setSearchTickers(data.tickers);
     });
+};
+
+export const getApiData = (props) => {
+  const {
+    tickers,
+    setEntireDomain,
+    setChartData,
+    setRenderLoad,
+    setWeights,
+  } = props;
+  getAllocation(tickers).then(
+    (alloc) => {
+      setWeights(alloc.allocation);
+      getBacktest(alloc).then(
+        (res) => {
+          const data = res.results.returns;
+          const final = [];
+          for (var i = 0; i < data.length; i++) {
+            final.push({ x: new Date(i), y: data[i] });
+          }
+          setEntireDomain({
+            y: [_.minBy(final, (d) => d.y).y, _.maxBy(final, (d) => d.y).y],
+            x: [final[0].x, _.last(final).x],
+          });
+          setChartData(final);
+          setRenderLoad(false);
+        },
+        (err) => {
+          console.log(err);
+          setRenderLoad(false);
+        },
+      );
+    },
+    (err) => {
+      console.log(err);
+      setRenderLoad(false);
+    },
+  );
+};
+
+export const getAllocation = (tickers) => {
+  const urlString = `${process.env.REACT_APP_DB_URL}/application/allocation`;
+  return new Promise((resolve, reject) => {
+    fetch(urlString, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ stocks: tickers }),
+    })
+      .then((res) => res.json())
+      .then(
+        (allocation) => {
+          resolve(allocation);
+        },
+        (err) => {
+          reject(err);
+        },
+      );
+  });
+};
+
+export const getBacktest = (allocation) => {
+  const urlString = `${process.env.REACT_APP_DB_URL}/application/backtest`;
+  return new Promise((resolve, reject) => {
+    fetch(urlString, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(allocation),
+    })
+      .then((res) => res.json())
+      .then(
+        (json) => {
+          resolve(json);
+        },
+        (err) => {
+          reject(err);
+        },
+      );
+  });
 };
